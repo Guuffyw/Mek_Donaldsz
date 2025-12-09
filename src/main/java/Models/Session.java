d246c1ec17716102507a375d0b81e5928ef64d83
Guuffyw <guuffy0681@gmail.com>
2025-12-09 14:07:45 +0100

    Update Session.java

--- src/main/java/Models/Session.java
+++ src/main/java/Models/Session.java
@@ -1,4 +1,30 @@
 package Models;
 
 public class Session {
+    private static String currentUsername;
+    private static boolean isLoggedIn = false;
+    private static int currentBalance;
+
+    public static void login(String username,int balance) {
+        currentBalance = balance;
+        currentUsername = username;
+        isLoggedIn = true;
+    }
+
+    public static void logout() {
+        currentBalance = 0;
+        currentUsername = null;
+        isLoggedIn = false;
+    }
+    public static int getCurrentBalance(){
+        return currentBalance;
+    }
+
+    public static String getCurrentUsername() {
+        return currentUsername;
+    }
+
+    public static boolean isLoggedIn() {
+        return isLoggedIn;
+    }
 }
