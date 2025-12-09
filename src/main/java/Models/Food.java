56ac7f82abbe4b0161ebba95610d8d226d1c2ebf
Guuffyw <guuffy0681@gmail.com>
2025-12-09 14:08:33 +0100

    Update Food.java

--- src/main/java/Models/Food.java
+++ src/main/java/Models/Food.java
@@ -1,4 +1,37 @@
 package Models;
 
 public class Food {
+    private String Name;
+    private int Quantity;
+    private int Price;
+
+    public Food(String name, int quantity, int price) {
+        Name = name;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
 }
