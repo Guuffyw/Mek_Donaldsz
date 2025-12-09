d27e30769737ca292cdfe8b178ad11526fead0c1
Guuffyw <guuffy0681@gmail.com>
2025-12-09 14:29:28 +0100

    Update SceneSwitcher.java

--- src/main/java/Utils/SceneSwitcher.java
+++ src/main/java/Utils/SceneSwitcher.java
@@ -1,4 +1,20 @@
-package Utils;
+package utils;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SceneSwitcher {
+
+    public static void switchScene(javafx.event.ActionEvent event, String fxmlPath) throws IOException {
+        Parent root = FXMLLoader.load(SceneSwitcher.class.getResource(fxmlPath));
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
