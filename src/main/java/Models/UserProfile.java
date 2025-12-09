739a513a730719b950fc6dee1748173fd3fa7220
Guuffyw <guuffy0681@gmail.com>
2025-12-09 14:06:54 +0100

    Update UserProfile.java

--- src/main/java/Models/UserProfile.java
+++ src/main/java/Models/UserProfile.java
@@ -1,4 +1,37 @@
 package Models;
 
+
 public class UserProfile {
+
+    private int id;
+    private int balance;
+    private String rank;
+
+    public UserProfile(int id, int balance, String rank) {
+        this.id = id;
+        this.balance = balance;
+        this.rank = rank;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
 }
+
